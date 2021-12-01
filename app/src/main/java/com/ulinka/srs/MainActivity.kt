f5eb83ee36294a7aaf4cb3e91c40e28fe7e21b54
Ulinka88 <ulinia@mail.ru>
2021-12-02 00:28:28 +0300

    FC

---
+++ app/src/main/java/com/ulinka/srs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ulinka.srs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
